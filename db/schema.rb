66371ff5a0469c8a3416bb4d953aa46378982fba
rosangela <roliveirasv@gmail.com>
2020-12-24 08:37:03 -0300

    Gerar scaffold despesa e inserir item no menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_235820) do
+ActiveRecord::Schema.define(version: 2020_12_24_113321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2020_12_23_235820) do
     t.string "nome"
     t.string "telefone"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "despesas", force: :cascade do |t|
+    t.string "nome"
+    t.string "descricao"
+    t.float "valor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
