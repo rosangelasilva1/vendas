94e83cd1a9f823d5853ddadee6af9a3ad4cb6f87
rosangela <roliveirasv@gmail.com>
2020-12-28 12:09:16 -0300

    rails db:migrate de pedido_produto

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_145949) do
+ActiveRecord::Schema.define(version: 2020_12_28_150801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 2020_12_28_145949) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "pedido_produtos", force: :cascade do |t|
+    t.bigint "pedido_id"
+    t.bigint "produto_id"
+    t.float "valor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pedido_id"], name: "index_pedido_produtos_on_pedido_id"
+    t.index ["produto_id"], name: "index_pedido_produtos_on_produto_id"
+  end
+
   create_table "pedidos", force: :cascade do |t|
     t.bigint "cliente_id"
     t.float "valor_total"
@@ -67,5 +77,7 @@ ActiveRecord::Schema.define(version: 2020_12_28_145949) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pedido_produtos", "pedidos"
+  add_foreign_key "pedido_produtos", "produtos"
   add_foreign_key "pedidos", "clientes"
 end
