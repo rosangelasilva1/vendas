1545fafc974aaeb9ec7b0c2e12375c0762c6ab07
rosangela <roliveirasv@gmail.com>
2020-12-29 13:04:23 -0300

    Alterar critério do fluxo de caixa para somar valores por mês

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
     def index
-        @receitas = Receita.sum(:valor)
-        @despesas = Despesa.sum(:valor)
-        @pedidos = Pedido.sum(:valor_total)
+        @receitas = Receita.where("created_at between ? and ?",Time.zone.now.beginning_of_month,Time.zone.now.end_of_month).sum(:valor)
+        @despesas = Despesa.where("created_at between ? and ?",Time.zone.now.beginning_of_month,Time.zone.now.end_of_month).sum(:valor)
+        @pedidos = Pedido.where("created_at between ? and ?",Time.zone.now.beginning_of_month,Time.zone.now.end_of_month).sum(:valor_total)
         @saldo = @receitas - @despesas + @pedidos
     end
 end
