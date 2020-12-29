e9d486670b48b2c9217903e08ab89e8725ba0fcc
rosangela <roliveirasv@gmail.com>
2020-12-28 23:43:32 -0300

    Resumo do fluxo de caixa na Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
     def index
+        @receitas = Receita.sum(:valor)
+        @despesas = Despesa.sum(:valor)
+        @pedidos = Pedido.sum(:valor_total)
+        @saldo = @receitas - @despesas + @pedidos
     end
 end
