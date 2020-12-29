b9f247295982c9de3ba8d7033c8876bf52ac5cb8
rosangela <roliveirasv@gmail.com>
2020-12-28 23:23:28 -0300

    Atualizar valor total do Pedido ao salvar item de pedido

--- app/models/pedido_produto.rb
+++ app/models/pedido_produto.rb
@@ -1,4 +1,10 @@
 class PedidoProduto < ApplicationRecord
   belongs_to :pedido
   belongs_to :produto
+  after_save :atualiza_valor_pedido
+
+  def atualiza_valor_pedido
+    self.pedido.valor_total = PedidoProduto.where(pedido_id: self.pedido_id).sum(:valor)
+    self.pedido.save!
+  end
 end
